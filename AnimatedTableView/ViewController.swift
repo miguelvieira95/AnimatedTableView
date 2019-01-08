7566e3b17b4ddf42b610c47154931e39b50c3b21
Miguel Vieira <miguel.vieira2@altran.com>
2019-01-08 13:25:04 +0000

    Initial Commit

---
+++ AnimatedTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedTableView
+//
+//  Created by Miguel Vieira on 08/01/2019.
+//  Copyright © 2019 Miguel Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AnimatedTableViewTests/AnimatedTableViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  AnimatedTableViewTests.swift
+//  AnimatedTableViewTests
+//
+//  Created by Miguel Vieira on 08/01/2019.
+//  Copyright © 2019 Miguel Vieira. All rights reserved.
+//
+
+import XCTest
+@testable import AnimatedTableView
+
+class AnimatedTableViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
